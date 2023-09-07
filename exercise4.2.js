c4d8d58e4116521bc96b6cc9c07b2b711417a2cf
Abagels96 <138115235+Abagels96@users.noreply.github.com>
2023-09-07 18:43:14 -0500

    hoisting

---
+++ exercise4.2.js
@@ -0,0 +1,9 @@
+function fineDay() {
+    console.log("It is a fine day")	// Call the hoistedFunction here
+        // Now, declare the hoistedFunction below
+    }
+    
+    
+fineDay();
+
+// didn't change a thing which way the declaration and definition were
