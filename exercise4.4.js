04ce1887695312b154bafcacd60eff3de904cc69
Abagels96 <138115235+Abagels96@users.noreply.github.com>
2023-09-07 20:19:44 -0500

    scope exercise 4

---
+++ exercise4.4.js
@@ -0,0 +1,14 @@
+let fan="All around the world";
+console.log(fan);
+
+function juice(){
+let fan="whir";
+}
+juice();
+console.log(fan);
+
+function song(){
+    let fan="there are many people over there";
+    console.log(fan);
+}
+song();
