0281192c13cb0402e653910de599579407ece440
Abagels96 <138115235+Abagels96@users.noreply.github.com>
2023-09-07 20:06:01 -0500

    outer and inner variables

---
+++ exercise4.3.js
@@ -0,0 +1,13 @@
+
+
+function firstDoll(){
+
+    let color1="brown";
+    console.log(color1);
+    function secondDoll(color1){
+        let color2="white";
+        console.log(color2,color1);
+    }
+    secondDoll();
+}
+firstDoll();
