f919ade9ac1c45586e7f4914e86948906331ce82
Abagels96 <138115235+Abagels96@users.noreply.github.com>
2023-09-09 17:49:55 -0500

    Variable declaration

---
+++ exercise4.6.js
@@ -0,0 +1,11 @@
+
+function testHoisting(){
+    let harp=21;
+    console.log(harp);
+   
+}
+
+testHoisting();
+
+// That didn't work at all. The console threw an error about variable declarations
+// you have got to declare a variable before you can use it. 
