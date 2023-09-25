a34689ecd8fe6f333e38c42796316bcd442e4d13
Abagels96 <138115235+Abagels96@users.noreply.github.com>
2023-09-25 10:10:27 -0500

    update

---
+++ exercise4.7.js
@@ -0,0 +1,16 @@
+function oneTwoThree(){
+    let count;
+    console.log(count);
+
+ function running(count){
+       for(count=0; count<10; count++){
+
+        console.log(count);}
+
+            }
+
+    running();
+}
+
+           oneTwoThree();
+    
---
+++ exercise4.9.js
@@ -0,0 +1,18 @@
+
+let hunger= true;
+
+
+function changeTheWorld(hunger){
+
+    let hunger=false;
+
+
+
+
+
+}
+
+function findProblems (war){
+war=false;
+let war=true;
+}
